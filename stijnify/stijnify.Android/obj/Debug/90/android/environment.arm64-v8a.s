db7552ab451c37f9ea3238d45f93904160566c45
Stijn Lingmont <80831@rocteraa-student.nl>
2020-11-04 10:32:37 +0100

    Removal most of unnacessary files

--- stijnify/stijnify.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ stijnify/stijnify.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"5a592e32-f6fa-45db-bf99-147c0e30c958"
+	.asciz	"782c0b76-846f-4959-a372-0909b46372be"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
