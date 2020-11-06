753e24003f8c8f03b2753ed3d9e45c6aae1cf28d
Stijn Lingmont <80831@rocteraa-student.nl>
2020-11-06 10:20:06 +0100

    Delete a folder

--- stijnify/stijnify/Views/SettingsPage.xaml.cs
+++ stijnify/stijnify/Views/SettingsPage.xaml.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -54,6 +55,44 @@ namespace stijnify.Views
                 addedFolders.ItemsSource = folderList;
             }
 
+        }
+
+        /// <summary>
+        /// Event for folder options
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void FolderDelete_Clicked(object sender, EventArgs e)
+        {
+            string selectedItem = ((ImageButton)sender).CommandParameter.ToString();
+            var response = await DisplayAlert("Deleting folder", "Are you sure that you want to delete this folder?", "Yes", "No");
+
+            //Check if user want to proceed
+            if (response)
+            {
+                //Delete the folder
+                List<string> folders = new List<string>();
+
+                //Retrieve the full string with data
+                var foldersString = Preferences.Get("folders", null);
+
+                //Check if list is found
+                if(foldersString == null)
+                    return;
+
+                //Convert the string in a list
+                folders = foldersString.Split(',').ToList();
+
+                //Find specefic folder
+                bool folderExcists = folders.Any(folder => folder == selectedItem);
+
+                if (folderExcists)
+                    folders.Remove(selectedItem);
+
+                //Set list in Prefferences
+                Preferences.Set("folders", string.Join(",", folders));
+                addedFolders.ItemsSource = folders;
+            }
         }
     }
 }
